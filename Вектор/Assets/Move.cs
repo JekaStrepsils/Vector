9abf09fe87149a8ad468f065178b67f256fe6d89
JekaStrepsils <chornai.denys1121@vu.cdu.edu.ua>
2024-04-28 12:07:35 +0300

    first

---
+++ Вектор/Assets/Move.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+    public GameObject goal;
+    Vector3 directiton;
+    float speed = 5f;
+
+    void Start() {
+
+        //directiton = goal.transform.position - this.transform.position;
+        
+        //this.transform.Translate(directiton);
+    }
+
+    private void LateUpdate() 
+    {
+        directiton = goal.transform.position - this.transform.position;
+        this.transform.LookAt(goal.transform.position);
+        if (directiton.magnitude > 2)
+        {
+            Vector3 velocity = directiton.normalized * speed * Time.deltaTime;
+            this.transform.position = this.transform.position + velocity;
+        }
+    }
+}
